d8fe026f820d5bc3d4226a416fbc0a21f7636ae4
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 08:41:11 +0300

    Q1

--- C44-G01-LINQ01/Program.cs
+++ C44-G01-LINQ01/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Problem 1: Find all products that are out of stock
+            {
+                Console.WriteLine("=== Products out of stock ===");
+                var outOfStockProducts = ListGenerators.ProductList.Where(p => p.UnitsInStock == 0);
+                foreach (var product in outOfStockProducts)
+                {
+                    Console.WriteLine($"{product.ProductName} - {product.UnitPrice:C}");
+                }
+                Console.WriteLine();
+            }
+            #endregion
         }
 
     }
